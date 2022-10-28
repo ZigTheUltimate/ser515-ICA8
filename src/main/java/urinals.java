0c905d1c604329afffcfbefb0858e516d150269f
Anmol More <amore9@asu.edu>
2022-10-27 20:57:29 -0700

    Added skeleton of important functions

--- src/main/java/urinals.java
+++ src/main/java/urinals.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class urinals {
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Anmol More
+ */
+
+public class urinals {
+
+    public static void main(String[] args) {
+
+        // TODO: Read input from file
+        List<Integer> input;
+        if (args[0] == null || args[0].trim().isEmpty()) {
+            System.out.println("You need to specify a path!");
+            return;
+        } else {
+            input = inputFromFile(args[0]);
+        }
+        // TODO: Count urinals
+        int output = countUrinals(input);
+
+        System.out.println(output);
+
+    }
+
+    private static int countUrinals(List<Integer> input) {
+        return -1;
+    }
+
+    protected static List<Integer> inputFromFile(String arg) {
+        return new ArrayList<>();
+    }
+
 }
--- src/test/java/TestJunit.java
+++ src/test/java/TestJunit.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class TestJUnit {
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestJunit {
+    @Test
+
+    public void testAdd() {
+        String str = "C:\\Users\\morea\\OneDrive\\Desktop\\input.txt";
+        assertEquals("Hello", Arrays.toString(urinals.inputFromFile(str).toArray()));
+    }
 }
